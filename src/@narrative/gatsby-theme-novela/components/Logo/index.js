b611bf97a62e731ab49dfc9c80423ecc68e143ac
Gangaprasad Mohite <gemohite@gmail.com>
2019-10-17 11:36:21 +0530

    added a logo

---
+++ src/@narrative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {link} from 'gatsby';
+
+/**
+ * Paste in your SVG logo and return it from this component.
+ * Make sure you have a height set for your logo.
+ * It is recommended to keep the height within 25-35px.
+ */
+export default function Logo() {
+  return (
+    <link to="/"><h1 style= {fontsize : '2.5rem'}>Shanky</h1></link>
+  );
+}
